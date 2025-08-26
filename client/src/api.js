297bc2a83513a5d8df217de85f64240b422b8f73
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-08-26 17:31:50 +0100

    update production api

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000';
+const API_BASE =
+    process.env.NODE_ENV === 'production' ? 'https://spotify-top-api.onrender.com/' : 'http://localhost:5000';
+// const API_BASE = 'http://localhost:5000';
 
 export const fetchTopTracks = accessToken => {
     return axios
